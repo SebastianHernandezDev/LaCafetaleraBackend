1eaf1952aeb1037e5ced64ef731d86f0659212d4
Ukpercival <miguelangelsimbaqueba10@gmail.com>
2025-09-18 10:33:07 -0500

    configuracion deploy AWS

---
+++ src/main/java/com/LaCafetalera/API_REST/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.LaCafetalera.API_REST.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Aplica a todas las rutas
+                        .allowedOrigins("https://lacafetaleraapp.netlify.app")  // Permite todos los orígenes (usa con precaución)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
