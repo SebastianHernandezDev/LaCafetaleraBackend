a0122810958ace8cfb19421c1ffbc6ec9aa161a9
Ukpercival <miguelangelsimbaqueba10@gmail.com>
2025-09-22 20:23:21 -0500

    SpringAplication  fixed: adding basic controller  test

--- src/main/java/com/LaCafetalera/API_REST/ApiRestApplication.java
+++ src/main/java/com/LaCafetalera/API_REST/ApiRestApplication.java
@@ -2,12 +2,20 @@ package com.LaCafetalera.API_REST;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
 
+
+	@GetMapping("/hello")  // <- Agregar este método
+	public String hello() {
+		return "Aplicación Spring Boot funcionando correctamente";
+	}
 }
